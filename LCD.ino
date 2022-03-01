d027fefb976add301ac33855c52b1356bb37b91e
giu11 <85152594+giu11@users.noreply.github.com>
2022-03-01 23:17:50 +0100

    Update LCD.ino

--- LCD.ino
+++ LCD.ino
@@ -1,6 +1,9 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
- 
+#define trigPin 6
+#define echo1Pin 7
+#define echo2Pin 13
+long durata, cm, cm2;
 void setup()
 {
 lcd.begin(16, 2);
@@ -12,6 +15,10 @@ pinMode(8, INPUT);
 digitalWrite(8, HIGH);
 pinMode(9, INPUT);
 digitalWrite(9, HIGH);
+pinMode(trigPin, OUTPUT);
+pinMode(echo1Pin, INPUT);
+pinMode(echo2Pin, OUTPUT);
+
 }
 double i = 0;
 double a = millis();
@@ -19,8 +26,15 @@ double c ;
  
 void loop()
 {
-lcd.clear();
-lcd.print("press start");
+  trig();
+  durata = pulseIn(echo1Pin, HIGH);
+  cm = durata / 58;
+  delayMicroseconds(50);
+  trig();
+  durata = pulseIn(echo2Pin, HIGH);
+  cm2 = durata / 58;
+  delayMicroseconds(50); 
+lcd.print("Posiziona l'oggetto");
 delay(100);
  
 if(digitalRead(8) == LOW)
@@ -28,7 +42,7 @@ if(digitalRead(8) == LOW)
  
 lcd.clear();
 a = millis();
-while(digitalRead(9) == HIGH)
+while(digitalRead(9) == HIGH && cm > 8 && cm < 15)
 {
  
 c = millis();
@@ -46,7 +60,7 @@ delay(100);
  
 if(digitalRead(9) == LOW)
 {
-while(digitalRead(8) == HIGH)
+while(digitalRead(8) == HIGH && cm2 > 3 && cm2 < 6)
 {
 lcd.setCursor(0,0);
 lcd.print(i);
@@ -57,4 +71,11 @@ delay(100);
 }
 }
 }
+}
+void trig () {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 }
